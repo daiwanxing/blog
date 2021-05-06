7f101a15c1660a197e0092171b96747bb348d33f
daiwanxing <daiwanxing@antvsion.com>
2021-05-06 09:27:17 +0800

    update: 事件循环内容补充

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,7 +3,14 @@ module.exports = {
     base: "/frontend-notes/",
     port: 8088,
     description: '用来记录自己平日的学习笔记',
+    locales: {
+        "/": {
+            lang: "zh-CN",
+            title: "前端学习小站"
+        }  
+    },
     themeConfig: {
+        lastUpdated: '上次更新时间',
         logo: "/logo.png",
         sidebarDepth: 2,
         sidebar: [
@@ -17,8 +24,9 @@ module.exports = {
                 sidebarDepth: 2
             },
             {
-                title: "js语法疏漏",
-                path: "/js/"
+                title: "js相关",
+                path: "/js/",
+                sidebarDepth: 2
             }
         ]
     }
