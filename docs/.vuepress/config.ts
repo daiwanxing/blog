a67c8cc25e83e60d4c817bea0a59223ea44aa5a0
daiwanxing <377099119@qq.com>
2022-06-21 23:06:36 +0800

    chore: 配置文件调整

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,5 +1,7 @@
 import type { SidebarConfig } from '@vuepress/theme-default';
-
+import { defineUserConfig } from "@vuepress/cli";
+import { viteBundler } from "@vuepress/bundler-vite";
+import { defaultTheme } from "@vuepress/theme-default";
 
 const sidebar: SidebarConfig = {
     '/guide': [
@@ -19,19 +21,7 @@ const sidebar: SidebarConfig = {
     ]
 }
 
-const themeConfig = {
-    darkMode: true,
-    lastUpdated: true,
-    lastUpdatedText: "上一次更新时间",
-    logo: "/logo.png",
-    repo: "https://github.com/daiwanxing/frontend-notes",
-    repoLabel: "GitHub",
-    sidebar: sidebar,
-    editLink: false,
-    contributors: false,
-}
-
-module.exports = {
+module.exports = defineUserConfig({
     title: '前端笔记小册',
     lang: "zh-CN",
     base: "/frontend-notes/",
@@ -40,20 +30,29 @@ module.exports = {
     locales: {
         "/": {
             lang: "zh-CN",
-            title: "前端笔记小册"
-        }  
+            title: "前端笔记小册",
+        },
     },
+    bundler: viteBundler(),
     head: [
-        ['link', { rel: 'icon', href: '/frontend-notes/whale-16x16.png', type: "image/png", sizes:"16x16" }],
-        ['link', { rel: 'icon', href: '/frontend-notes/fav.png', type: "image/png", sizes:"32x32" }],
-        ['meta', { name: "viewport", content: "width=device-width, initial-scale=1.0"}]
+        ['link', { rel: 'icon', href: '/frontend-notes/whale-16x16.png', type: "image/png", sizes: "16x16" }],
+        ['link', { rel: 'icon', href: '/frontend-notes/fav.png', type: "image/png", sizes: "32x32" }],
+        ['meta', { name: "viewport", content: "width=device-width, initial-scale=1.0" }]
     ],
     // themeConfig 的配置项属于主题配置
-    themeConfig,
+    theme: defaultTheme({
+        lastUpdated: true,
+        lastUpdatedText: "上一次更新时间",
+        logo: "/logo.png",
+        repo: "https://github.com/daiwanxing/frontend-notes",
+        repoLabel: "GitHub",
+        sidebar: sidebar,
+        editLink: false,
+        contributors: false,
+    }),
     plugins: [
-        ['@vuepress/search', {
-            searchMaxSuggestions: 10
-        }],
-
+        // ['@vuepress/search', {
+        //     searchMaxSuggestions: 10
+        // }],
     ],
-}
+});
