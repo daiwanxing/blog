8231c68e174dec0abf8e017020d0f35c3e8560aa
daiwanxing <377099119@qq.com>
2022-12-28 23:35:41 +0800

    docs: add 网站图标

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,59 +1,92 @@
 export default {
-  title: "daiwanxing的日志",
-  lang: "zh-CN",
-  lastUpdated: true,
-  base: "/fn/",
-  description:
-    "一个developer将这几年开发生涯中走过的弯路、学到的技术以及总结的经验写下来给自己看的一本碎碎念的日志",
-  themeConfig: {
-    lastUpdatedText: "最近一次更新于",
-    sidebar: {
-      "/js/": [
-        {
-          text: "javascript",
-          items: [
+    title: "daiwanxing的日志",
+    lang: "zh-CN",
+    lastUpdated: true,
+    base: "/fn/",
+    description:
+        "一个developer将这几年开发生涯中走过的弯路、学到的技术以及总结的经验写下来给自己看的一本碎碎念的日志",
+    head: [
+        [
+            "link",
             {
-              text: "聊聊requestAnimationFrame",
-              link: "/js/requestAnimationFrame",
+                rel: "icon",
+                href: "/logo-32x32.ico",
+                type: "image/x-icon",
+                size: "32x32",
             },
+        ],
+        [
+            "link",
             {
-              text: "WeakMap&WeakSet",
-              link: "/js/WeakMap&WeakSet",
+                rel: "icon",
+                href: "/logo-64x64.ico",
+                type: "image/x-icon",
+                size: "64x64",
             },
-          ],
-        },
-      ],
-      "/ts/": [
-        {
-          text: "typescript",
-          items: [
+        ],
+        [
+            "link",
             {
-              text: "typescript练习题",
-              link: "/ts/",
+                rel: "icon",
+                href: "/logo-128x128.ico",
+                type: "image/x-icon",
+                size: "128x128",
             },
-            {
-              text: "如何配置tsconfig.json",
-              link: "/ts/tsconfig.md",
-            },
-            {
-              text: "declare关键字的使用",
-              link: "/ts/declare.md"
-            },
-            {
-              text: "三斜线指令是什么",
-              link: "/ts/three-triple-line.md"
-            }
-          ],
-        },
-      ],
-    },
-    nav: [
-      { text: "javascript", link: "/js/", activeMatch: "/js/" },
-      { text: "typescript", link: "/ts/", activeMatch: "/ts/" },
-      { text: "css", link: "https://github.com/..." },
+        ],
+        [
+            "link",
+            { rel: "apple-touch-icon", href: "/logo-32x32.ico", size: "32x32" },
+        ],
     ],
-    socialLinks: [
-      { icon: 'github', link: 'https://github.com/daiwanxing' },
-    ]
-  },
+    themeConfig: {
+        lastUpdatedText: "最近一次更新于",
+        sidebar: {
+            "/js/": [
+                {
+                    text: "javascript",
+                    items: [
+                        {
+                            text: "聊聊requestAnimationFrame",
+                            link: "/js/requestAnimationFrame",
+                        },
+                        {
+                            text: "WeakMap&WeakSet",
+                            link: "/js/WeakMap&WeakSet",
+                        },
+                    ],
+                },
+            ],
+            "/ts/": [
+                {
+                    text: "typescript",
+                    items: [
+                        {
+                            text: "typescript练习题",
+                            link: "/ts/",
+                        },
+                        {
+                            text: "如何配置tsconfig.json",
+                            link: "/ts/tsconfig.md",
+                        },
+                        {
+                            text: "declare关键字的使用",
+                            link: "/ts/declare.md",
+                        },
+                        {
+                            text: "三斜线指令是什么",
+                            link: "/ts/three-triple-line.md",
+                        },
+                    ],
+                },
+            ],
+        },
+        nav: [
+            { text: "javascript", link: "/js/", activeMatch: "/js/" },
+            { text: "typescript", link: "/ts/", activeMatch: "/ts/" },
+            { text: "css", link: "https://github.com/..." },
+        ],
+        socialLinks: [
+            { icon: "github", link: "https://github.com/daiwanxing" },
+        ],
+    },
 };
--- docs/index.md
+++ docs/index.md
@@ -1,17 +1,25 @@
----   
+---
 layout: home
 
+features:
+    - icon: 🧠
+      title: Learn
+    - icon: 🖖
+      title: Think
+    - icon: 🛠️
+      title: improve
+
 hero:
-  name: daiwanxing的日志
-  text: 让记录成为一种良好的习惯.
-  tagline: 一个developer将这几年开发生涯中走过的弯路、学到的技术以及总结的经验写下来给自己看的一本碎碎念的日志
-  image:
-    src: /logo.png
-  actions:
-    - theme: brand
-      text: 立即阅读
-      link: /js/requestAnimationFrame
-    - theme: alt
-      text: 在github上查看
-      link: https://github.com/vuejs/vitepress
+    name: daiwanxing的日志
+    text: 让记录成为一种良好的习惯.
+    tagline: 一个developer将这几年开发生涯中走过的弯路、学到的技术以及总结的经验写下来给自己看的一本碎碎念的日志
+    image:
+        src: /logo.png
+    actions:
+        - theme: brand
+          text: 立即阅读
+          link: /js/requestAnimationFrame
+        - theme: alt
+          text: 在github上查看
+          link: https://github.com/vuejs/vitepress
 ---
