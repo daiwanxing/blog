85d152efdf94196257617f37bb844e6f3d2cde8f
daiwanxing <377099119@qq.com>
2022-12-29 00:00:45 +0800

    chore: ignore dead link

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,92 +1,91 @@
 export default {
-    title: "daiwanxing的日志",
-    lang: "zh-CN",
-    lastUpdated: true,
-    base: "/frontend-notes/",
-    description:
-        "一个developer将这几年开发生涯中走过的弯路、学到的技术以及总结的经验写下来给自己看的一本碎碎念的日志",
-    head: [
-        [
-            "link",
+   title: "daiwanxing的日志",
+   lang: "zh-CN",
+   lastUpdated: true,
+   ignoreDeadLinks: true,
+   base: "/frontend-notes/",
+   description:
+      "一个developer将这几年开发生涯中走过的弯路、学到的技术以及总结的经验写下来给自己看的一本碎碎念的日志",
+   head: [
+      [
+         "link",
+         {
+            rel: "icon",
+            href: "/logo-32x32.ico",
+            type: "image/x-icon",
+            size: "32x32",
+         },
+      ],
+      [
+         "link",
+         {
+            rel: "icon",
+            href: "/logo-64x64.ico",
+            type: "image/x-icon",
+            size: "64x64",
+         },
+      ],
+      [
+         "link",
+         {
+            rel: "icon",
+            href: "/logo-128x128.ico",
+            type: "image/x-icon",
+            size: "128x128",
+         },
+      ],
+      [
+         "link",
+         { rel: "apple-touch-icon", href: "/logo-32x32.ico", size: "32x32" },
+      ],
+   ],
+   themeConfig: {
+      lastUpdatedText: "最近一次更新于",
+      sidebar: {
+         "/js/": [
             {
-                rel: "icon",
-                href: "/logo-32x32.ico",
-                type: "image/x-icon",
-                size: "32x32",
+               text: "javascript",
+               items: [
+                  {
+                     text: "聊聊requestAnimationFrame",
+                     link: "/js/requestAnimationFrame",
+                  },
+                  {
+                     text: "WeakMap&WeakSet",
+                     link: "/js/WeakMap&WeakSet",
+                  },
+               ],
             },
-        ],
-        [
-            "link",
+         ],
+         "/ts/": [
             {
-                rel: "icon",
-                href: "/logo-64x64.ico",
-                type: "image/x-icon",
-                size: "64x64",
+               text: "typescript",
+               items: [
+                  {
+                     text: "typescript练习题",
+                     link: "/ts/",
+                  },
+                  {
+                     text: "如何配置tsconfig.json",
+                     link: "/ts/tsconfig.md",
+                  },
+                  {
+                     text: "declare关键字的使用",
+                     link: "/ts/declare.md",
+                  },
+                  {
+                     text: "三斜线指令是什么",
+                     link: "/ts/three-triple-line.md",
+                  },
+               ],
             },
-        ],
-        [
-            "link",
-            {
-                rel: "icon",
-                href: "/logo-128x128.ico",
-                type: "image/x-icon",
-                size: "128x128",
-            },
-        ],
-        [
-            "link",
-            { rel: "apple-touch-icon", href: "/logo-32x32.ico", size: "32x32" },
-        ],
-    ],
-    themeConfig: {
-        lastUpdatedText: "最近一次更新于",
-        sidebar: {
-            "/js/": [
-                {
-                    text: "javascript",
-                    items: [
-                        {
-                            text: "聊聊requestAnimationFrame",
-                            link: "/js/requestAnimationFrame",
-                        },
-                        {
-                            text: "WeakMap&WeakSet",
-                            link: "/js/WeakMap&WeakSet",
-                        },
-                    ],
-                },
-            ],
-            "/ts/": [
-                {
-                    text: "typescript",
-                    items: [
-                        {
-                            text: "typescript练习题",
-                            link: "/ts/",
-                        },
-                        {
-                            text: "如何配置tsconfig.json",
-                            link: "/ts/tsconfig.md",
-                        },
-                        {
-                            text: "declare关键字的使用",
-                            link: "/ts/declare.md",
-                        },
-                        {
-                            text: "三斜线指令是什么",
-                            link: "/ts/three-triple-line.md",
-                        },
-                    ],
-                },
-            ],
-        },
-        nav: [
-            { text: "javascript", link: "/js/", activeMatch: "/js/" },
-            { text: "typescript", link: "/ts/", activeMatch: "/ts/" },
-            { text: "css", link: "https://github.com/..." },
-        ],
-        socialLinks: [
-            { icon: "github", link: "https://github.com/daiwanxing" },
-        ],
-    },
+         ],
+      },
+      nav: [
+         { text: "javascript", link: "/js/", activeMatch: "/js/" },
+         { text: "typescript", link: "/ts/", activeMatch: "/ts/" },
+         { text: "css", link: "https://github.com/..." },
+      ],
+      socialLinks: [{ icon: "github", link: "https://github.com/daiwanxing" }],
+   },
 };
