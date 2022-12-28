0e950d41581aa41112414e310227d9bc7b9d82d0
daiwanxing <377099119@qq.com>
2022-12-28 23:36:23 +0800

    docs: 配置base

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -2,7 +2,7 @@ export default {
     title: "daiwanxing的日志",
     lang: "zh-CN",
     lastUpdated: true,
-    base: "/fn/",
+    base: "/frontend-notes/",
     description:
         "一个developer将这几年开发生涯中走过的弯路、学到的技术以及总结的经验写下来给自己看的一本碎碎念的日志",
     head: [
