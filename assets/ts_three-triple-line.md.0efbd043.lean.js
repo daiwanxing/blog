2bfc0c74a2f4c3ada9f3519660793ff7953d27a6
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2023-01-08 08:14:59 +0000

    Deploy daiwanxing/frontend-notes to github.com/daiwanxing/frontend-notes.git:gh-pages

---
+++ assets/ts_three-triple-line.md.0efbd043.lean.js
@@ -0,0 +1,1 @@
+import{_ as s,c as e,o as t,a}from"./app.9573497a.js";const h=JSON.parse('{"title":"三斜线指令是什么?","description":"","frontmatter":{},"headers":[],"relativePath":"ts/three-triple-line.md","lastUpdated":1672240193000}'),l={name:"ts/three-triple-line.md"},n=a("",6),o=[n];function p(c,i,r,d,_,y){return t(),e("div",null,o)}const u=s(l,[["render",p]]);export{h as __pageData,u as default};
