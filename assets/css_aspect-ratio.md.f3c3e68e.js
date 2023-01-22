b53b407f853acd99d110ba7cbc59eb3198a356c0
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2023-01-22 11:44:05 +0000

    Deploy daiwanxing/frontend-notes to github.com/daiwanxing/frontend-notes.git:gh-pages

---
+++ assets/css_aspect-ratio.md.f3c3e68e.js
@@ -0,0 +1,1 @@
+import{_ as t,c as e,o as a}from"./app.257604ca.js";const m=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"css/aspect-ratio.md","lastUpdated":1674387702000}'),s={name:"css/aspect-ratio.md"};function c(o,r,n,p,i,d){return a(),e("div")}const f=t(s,[["render",c]]);export{m as __pageData,f as default};
--- assets/js_web-worker.md.df84b9f9.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as s,c as a,o as n,a as l}from"./app.da460f7b.js";const C=JSON.parse('{"title":"Web Worker","description":"","frontmatter":{},"headers":[],"relativePath":"js/web-worker.md","lastUpdated":1672135816000}'),o={name:"js/web-worker.md"},p=l("",7),e=[p];function t(r,c,y,D,F,A){return n(),a("div",null,e)}const w=s(o,[["render",t]]);export{C as __pageData,w as default};
