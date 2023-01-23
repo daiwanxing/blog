800b9e9e0c00a001f0083513ded03d50de3c1d6f
daiwanxing <377099119@qq.com>
2023-01-23 11:39:41 +0800

    docs: update aspect-ratio

--- docs/css/table-contents.js
+++ docs/css/table-contents.js
@@ -6,6 +6,10 @@ export default [
             text: "css字体的那些事儿",
             link: "/css/font-family",
          },
+         {
+            text: "aspect-radio",
+            link: "/css/aspect-ratio"
+         }
       ],
    },
 ];
