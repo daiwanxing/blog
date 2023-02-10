14bb5d2011836d68d03530264dc4e3f7f832797f
daiwanxing <37327614+daiwanxing@users.noreply.github.com>
2023-02-10 09:04:35 +0000

    Deploying to gh-pages from @ daiwanxing/blog@2b42820aeef919bb42ddc36ca9ce73b46d100eee 🚀

---
+++ assets/js_async-await.md.434f8066.lean.js
@@ -0,0 +1,1 @@
+import{_ as s,c as a,o as n,a as l}from"./app.cb61443c.js";const A=JSON.parse('{"title":"async - await","description":"","frontmatter":{},"headers":[],"relativePath":"js/async-await.md","lastUpdated":1676019626000}'),p={name:"js/async-await.md"},o=l("",3),e=[o];function t(c,r,y,F,D,i){return n(),a("div",null,e)}const d=s(p,[["render",t]]);export{A as __pageData,d as default};
