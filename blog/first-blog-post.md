a9e0e35f0e9334e7a6c2681fcf08dd9a2f484a3d
daiwanxing <wanxing.dai@coollu.com.cn>
2024-07-01 16:57:14 +0800

    feat: init commit

---
+++ blog/first-blog-post.md
@@ -0,0 +1,22 @@
+---
+slug: first-blog-post
+title: 从 Vitepress 到 Docusaurus
+authors:
+  name: daiwanxing
+  title: freelancer
+  url: https://github.com/daiwanxing
+  image_url: https://avatars.githubusercontent.com/u/37327614?v=4
+tags: [Vite, Docusaurus, React]
+---
+
+今天（也就是 2024年7月1日）决定将一年前用 [Vitepres](https://vitepress.dev/) 搭建的一个技术日志网站迁移至 [Docusaurus](https://tutorial.docusaurus.io/)。迁移的原因可能是自己爱折腾，这两个 SSG 框架我觉得都挺优秀的。无论是文档上手还是 UI 设计都深得我心。
+
+也有一部分原因是最近一直在恶补 React 知识（[Docusaurus](https://tutorial.docusaurus.io/) 也恰好是基于 React 开发的），从事前端行业近 5 年来写了太多的 Vue.js 项目了，相关的生态我也都用得如鱼得水。
+
+唯独 React 一直没有接触过，倒不是我多么排斥 React，纯粹是自己的工作上很少用到。
+
+我知道，想进步最好的办法就是勇敢的去跳出舒适圈，去用于尝试自己曾经未涉猎的事物，才能得到成长。
+
+所以也是借此机会希望这种折腾能够增进我的 React 功底。
+
+anyway, wish me good luck.
---
+++ docusaurus.config.ts
@@ -0,0 +1,111 @@
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
+
+const config: Config = {
+  title: "Klein's Diary",
+  tagline: "Klein 的技术日志",
+  favicon: "img/favicon.ico",
+
+  // Set the production url of your site here
+  url: "https://your-docusaurus-site.example.com",
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: "/",
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: "facebook", // Usually your GitHub org/user name.
+  projectName: "docusaurus", // Usually your repo name.
+
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"],
+  },
+
+  presets: [
+    [
+      "classic",
+      {
+        docs: {
+          sidebarPath: "./sidebars.ts",
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+        },
+        theme: {
+          customCss: "./src/css/custom.css",
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    image: "img/docusaurus-social-card.jpg",
+    navbar: {
+      title: `Klein's Diary`,
+      logo: {
+        alt: "My Site Logo",
+        src: "img/logo.svg",
+      },
+      items: [
+        {
+          type: "docSidebar",
+          sidebarId: "tutorialSidebar",
+          position: "left",
+          label: "Tutorial",
+        },
+        { to: "/blog", label: "Blog", position: "left" },
+        {
+          href: "https://github.com/facebook/docusaurus",
+          label: "GitHub",
+          position: "right",
+        },
+      ],
+    },
+    footer: {
+      style: "dark",
+      links: [
+        {
+          title: "Social Media",
+          items: [
+            {
+              label: "掘金",
+              href: "https://juejin.cn/user/4031266619656920",
+            },
+            {
+              label: "Github",
+              href: "https://github.com/daiwanxing",
+            },
+            {
+              label: "X",
+              href: "https://x.com/daiwxing/with_replies",
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Klein's Diary, Inc. Built with Docusaurus.`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
